009cd1826172919f13e3c1917ad6eea8e4a5f9eb
EverettTucker471 <eftucker@ncsu.edu>
2026-05-02 20:16:36 -0400

    Adding rendering functionality

--- gym_battlesnake/example_singleagent.py
+++ gym_battlesnake/example_singleagent.py
@@ -1,4 +1,5 @@
 import torch
+import time
 
 from gymbattlesnake import BattlesnakeEnv
 from custom_policy import CustomPolicy
@@ -7,29 +8,26 @@ from custom_policy import CustomPolicy
 from stable_baselines3 import PPO
 
 # Grabbing GPU device
-device = torch.rand(1).device
-print(device)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using {device}!")
+
 env = BattlesnakeEnv(n_threads=4, n_envs=16, device=device)
-model_name = "ppo_trained_model"
+model_name = "ppo_trained_model.zip"
 policy_kwargs = dict(
     features_extractor_class=CustomPolicy,
     features_extractor_kwargs=dict(features_dim=128),
 )
 
-def train():
+def evaluate():
     model = PPO("CnnPolicy", env, policy_kwargs=policy_kwargs, device=device, verbose=1)
-    model.learn(total_timesteps=100000)
+    model.learn(total_timesteps=100)
+
     model.save(model_name)
-
-
-def load():
-    model = PPO.load(model_name, device=device)
 
     obs = env.reset()
     for _ in range(10000):
         action,_ = model.predict(obs)
-        obs,_,_,_,_ = env.step(action)
+        obs,_,_,_ = env.step(action)
         env.render()
 
-train()
-load()
+evaluate()
